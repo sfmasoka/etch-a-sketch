7af196b768f0d13fc9acd1bef574e17e5c52b795
Sithembiso <fsmasoka@gmail.com>
2023-06-10 21:19:36 +0200

    added buttons and hover effect

--- javascript.js
+++ javascript.js
@@ -1,8 +1,28 @@
 const sketch_board = document.querySelector(".sketch-board");
 
 for(let i = 0; i<256;i++){
-    const  div = document.createElement("div");
+    const div = document.createElement("div");
     div.classList.add("grid");
     sketch_board.appendChild(div);
 
+    //dragover
+    div.addEventListener("mousemove",()=>
+{
+    div.classList.add("bg-color");
+});
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click",()=>
+{
+    div.style.cssText = "background-color: white;"
+});
+
+
+const eraser = document.querySelector(".clear");
+    eraser.addEventListener("click",()=>
+    {
+        div.style.cssText = "background-color: white;"
+    });
+
 }
+
